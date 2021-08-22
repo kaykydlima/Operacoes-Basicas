2f8c63fba08f531194e1c28f44d80ea8e2a2736d
kaykydlima <kayky140703@gmail.com>
2021-08-22 20:44:55 -0300

    matematicaBasica

---
+++ src/opbasicas/OpBasicas.java
@@ -0,0 +1,25 @@
+
+package opbasicas;
+
+public class OpBasicas {
+
+    
+    public static void main(String[] args) {
+        // Programa de operações básicas
+        //declaração das variáveis
+        int a = 4;
+        int b = 10;
+        
+        //calculos
+        double soma = a + b;
+        System.out.println("O valor da soma é: " + soma);
+        double subtracao = a - b;
+        System.out.println("O valor da subtração é: " + subtracao);
+        double multiplicacao = a * b;
+        System.out.println("O valor da multiplicação é: " + multiplicacao);
+        double divisao = (double) a / b;
+        System.out.println("O valor da divisão é: " + divisao);
+        
+    }
+    
+}
